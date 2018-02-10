1b0b94a542e17982716da5abe106963858320d98
Jennifer Lim <contact@jendigitalvision.com>
2018-02-10 10:05:17 +0100

    US-02: Improve code with subcomponent, services, global constant...

---
+++ react-ui/src/app/services/apiSuperHeroInfo/index.js
@@ -0,0 +1,10 @@
+import crypto from 'crypto';
+import { TIMESTAMP, API_PRIVATE, API_PUBLIC } from '../../env/global_constants';
+
+export default function apiUrlSuperHeroInfo(id) {
+    let tsprivkeypubkey = TIMESTAMP + API_PRIVATE + API_PUBLIC;
+    let hash = crypto.createHash('md5').update(tsprivkeypubkey).digest('hex');
+    let apiUrl = 'http://gateway.marvel.com/v1/public/characters/' + id + '?ts=' + TIMESTAMP + '&apikey=' + API_PUBLIC + '&hash=' + hash;
+
+    return apiUrl;
+}
